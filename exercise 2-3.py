045305f8c3707941f17d3389c65278570a6cb8e2
PatrickStenvik <75360650+PatrickStenvik@users.noreply.github.com>
2022-01-30 14:21:44 +0200

    works

---
+++ exercise 2-3.py
@@ -0,0 +1,19 @@
+
+
+x = int(input("Anna tehtyjen tehtävien määrä: "))
+if (x < 0):
+    print("Error: exercise points cannot be < 0 or > 120.")
+elif (x <= 60):
+    print("arvosanasi on: 0")
+elif (x <= 72):
+    print("arvosanasi on: 1")
+elif (x <= 84):
+    print("arvosanasi on: 2")
+elif (x <= 96):
+    print("arvosanasi on: 3")
+elif (x <= 108):
+    print("arvosanasi on: 4")
+elif (x <= 120):
+    print("arvosanasi on: 5")
+else:
+    print("Error: exercise points cannot be < 0 or > 120.")
